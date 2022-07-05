ed22a0109585de872844f152b9dcd6f931053d9c
Antonio Pasquale <antonio.pasquale@fmiworks.com>
2022-07-05 20:30:56 +1000

    Added test AssemblyInfo.cs

---
+++ AssemblyInfo.cs
@@ -0,0 +1,49 @@
+
+using FMI.WSMplannet.FMICommon;
+using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using System.Data;
+using System.Collections;
+using FMI.WSMplannet.DTO;
+using FMI.WSMplannet;
+
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly:AssemblyTitle("BusinessObjects")]
+[assembly:AssemblyDescription("")]
+[assembly:AssemblyCompany("Pasq Test Git")]
+[assembly:AssemblyProduct("BusinessObjects")]
+[assembly:AssemblyCopyright("Copyright © Pasq Test Git 2012")]
+[assembly:AssemblyTrademark("")]
+
+[assembly:ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly:Guid("0fc5ca93-5a87-46d3-a0f9-0123456789")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// <Assembly: AssemblyVersion("1.00.0.0")>
+
+[assembly:AssemblyVersion("1.00.0.0")]
+[assembly:AssemblyFileVersion("1.00.0.0")]
+
